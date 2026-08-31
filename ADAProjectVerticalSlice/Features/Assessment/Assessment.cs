d8a6e055b980fce272b98874db26e5ad96c77982
ninnalundberg <nllu71404@edu.ucl.dk>
2026-08-31 19:22:17 +0200

    "Opret Måling" implementeret i Vertical Slice Architecture. Minimal API, MediatR og CQRS

---
+++ ADAProjectVerticalSlice/Features/Assessment/Assessment.cs
@@ -0,0 +1,13 @@
+﻿namespace ADAProjectAPIVerticalSlice.Features.Assessment
+{
+    public class Assessment
+    {
+        public Guid AssessmentId { get; set; }
+
+        public string AssessmentName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
