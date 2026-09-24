0d6f4fc28201e3edf09affafbc3a5b28d99dbd0d
ninnalundberg <nllu71404@edu.ucl.dk>
2026-09-24 11:26:28 +0200

    rettelse i User entity - tilføjet JobTitle

--- ADAProjectVerticalSlice/Entities/User.cs
+++ ADAProjectVerticalSlice/Entities/User.cs
@@ -4,12 +4,13 @@ namespace ADAProjectAPIVerticalSlice.Entities
 {
     public class User : IdentityUser
     {
-        public string FullName { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string JobTitle { get; set; } = string.Empty;
 
         //Foreign key
         public Guid CompanyId { get; set; }
 
         //Navigation property
-        public Company Company { get; set; } = null;
+        public Company Company { get; set; } = null!;
     }
 }
